eba1c85007edd7a502733213ac8b7c9b67957783
scottlever99 <114246587+scottlever99@users.noreply.github.com>
2023-05-19 19:24:19 +0100

    Add project files.

---
+++ EcomClubDiscordPayment/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EcomClubDiscordPayment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EcomClubDiscordPayment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StripeDirect()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EcomClubDiscordPayment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcomClubDiscordPayment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
