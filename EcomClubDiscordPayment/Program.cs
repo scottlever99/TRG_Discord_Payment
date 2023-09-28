41bc221b4817ad2687b76ae19f6fd08aba0c3416
scottlever99 <114246587+scottlever99@users.noreply.github.com>
2023-09-28 19:55:15 +0100

    test

--- EcomClubDiscordPayment/Program.cs
+++ EcomClubDiscordPayment/Program.cs
@@ -20,6 +20,7 @@ namespace EcomClubDiscordPayment
             builder.Services.AddScoped<EmailService>();
 
 
+
             //TEst Stripe = "sk_test_51MygchAlSoLxBcHKjc6T16QCWcuHrZTOeJ2l67Fr4EbdbSLpwtzEXTpV6mAp0DkMnQV6G8o2SbVjZYF3VwtgIVFI00yjhgK614",
             //TEst Live Stripe = sk_test_51I88jPHy10dRIZQuiWpZ4xP3CSkYB6iOCiAzD9zXK4MJUbGHmeZiHGo0Qzz8UApg96GYWZi4lomzG1oaiCL8jZbI00m6HZrrTY 
             //Prod = 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
